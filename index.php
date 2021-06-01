c598f9582e271e0e29a35b59577416d248edc305
viswanathsitit <85150896+viswanathsitit@users.noreply.github.com>
2021-06-01 11:04:21 +0530

    Add files via upload
    UPDATED ON 01/JUNE/21

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>
+			ANNAUNIVERISTY RESULTS - AURESULTS
+		</title>
+        <style>
+            .resp-container 
+            {
+                position: relative;
+                overflow: hidden;
+                padding-top: 56.25%;
+            }
+        </style>
+	</head>
+	<body>
+        <center>
+            <!--
+            <div class="resp-container">
+                <iframe class="resp-iframe" src="http://annaunivv.blogspot.com/" gesture="media"  allow="encrypted-media" allowfullscreen></iframe> 
+            </div>
+            -->
+            <iframe src="http://annaunivv.blogspot.com/" width="90%" height="99%" frameborder="0" align="center" scrolling="auto"></iframe>
+        </center>
+	</body>
+</html>
